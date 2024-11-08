49dc4f9e35842a27e2597c2df774fc1e5decb9ac
Yiming0576 <Yiming0576@gmail.com>
2024-11-07 21:35:23 -0500

    add a method for getting input

--- src/main/java/com/pluralsight/utils/Utility.java
+++ src/main/java/com/pluralsight/utils/Utility.java
@@ -1,6 +1,26 @@
 package com.pluralsight.utils;
 
+import java.util.Scanner;
+
 public class Utility {
 
+
+    private static final Scanner input = new Scanner(System.in);
+
+    public static String getInputAsStringWithPrompt(String prompt) {
+        System.out.println(prompt);
+        String value = null;
+        try {
+            value = "";
+            do {
+                value = input.nextLine().trim();
+
+            } while (value.isEmpty() && value.isBlank());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return value;
+    }
 
 }
