9aa6f118b0d647fa71ee5eb48c6e27c63a538f19
Yiming0576 <Yiming0576@gmail.com>
2024-11-08 16:11:30 -0500

    doc the Command interface

---
+++ src/main/java/com/pluralsight/service/Command.java
@@ -0,0 +1,15 @@
+package com.pluralsight.service;
+
+
+//  trying to learn and implement the command pattern
+
+/**
+ * Interface representing a command that can be executed.
+ */
+public interface Command {
+
+    /**
+     * Executes the command.
+     */
+    void execute();
+}
