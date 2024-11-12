990542b27d6eed9f253b7e0d1681a9777bdb33b4
Yiming0576 <Yiming0576@gmail.com>
2024-11-12 11:19:03 -0500

    doc the class

--- src/main/java/com/pluralsight/entity/Food.java
+++ src/main/java/com/pluralsight/entity/Food.java
@@ -1,20 +1,53 @@
 package com.pluralsight.entity;
 
-
 import com.pluralsight.entity.sandwich.types.Size;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+/**
+ * Abstract class representing a food item.
+ */
 @Data
-public class Food {
+public abstract class Food {
 
+    /**
+     * The size of the food item.
+     * Must be specified.
+     */
     @NotNull(message = "Size must be specified")
     private Size size;
 
-    @Min(value = 0, message = "Price cannot lower than 0")
+    /**
+     * The price of the food item.
+     * Must be at least 0 and not exceed 100000.
+     */
+    @Min(value = 0, message = "Price cannot be lower than 0")
     @Min(value = 100000, message = "I do not have enough supply to make your Order")
     private double price;
 
+    /**
+     * The calories of the food item.
+     * Must not be empty and must be at least 0.
+     */
+    @NotEmpty
+    @Min(0)
+    private double calories;
+
+    /**
+     * Default constructor.
+     */
+    public Food() {
+    }
+
+    /**
+     * Constructor to create a food item with a specified size.
+     *
+     * @param size the size of the food item
+     */
+    public Food(Size size) {
+        this.size = size;
+    }
 }
