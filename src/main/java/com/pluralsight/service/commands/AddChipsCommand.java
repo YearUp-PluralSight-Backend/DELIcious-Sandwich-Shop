aab2664f190dfe3b190f10a106c28464217736fd
Yiming0576 <Yiming0576@gmail.com>
2024-11-12 14:00:05 -0500

    fix validation in addChipsCommand

--- src/main/java/com/pluralsight/service/commands/AddChipsCommand.java
+++ src/main/java/com/pluralsight/service/commands/AddChipsCommand.java
@@ -2,8 +2,11 @@ package com.pluralsight.service.commands;
 
 import com.pluralsight.entity.Food;
 import com.pluralsight.entity.Order;
+import com.pluralsight.entity.otherfood.ChipBrand;
 import com.pluralsight.entity.otherfood.Chips;
+import com.pluralsight.entity.sandwich.types.Size;
 import com.pluralsight.service.Command;
+import com.pluralsight.utils.ConstantValue;
 import com.pluralsight.utils.Utility;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,6 +17,7 @@ import org.apache.logging.log4j.Logger;
 public class AddChipsCommand implements Command {
 
     private final Order order;
+    private final Chips chips = new Chips();
     private final Logger logger = LogManager.getLogger(AddChipsCommand.class);
 
     /**
@@ -23,6 +27,28 @@ public class AddChipsCommand implements Command {
      */
     public AddChipsCommand(Order order) {
         this.order = order;
+        initializedChipsAttributes();
+
+    }
+
+    /**
+     * Initializes the attributes of the chips.
+     */
+    private void initializedChipsAttributes() {
+        ChipBrand chipBrand = null;
+        while (chipBrand == null) {
+            try {
+                Utility.print.accept(ConstantValue.CHIPS_MENU);
+                int option = Utility.getInputAsIntegerWithPrompt("->");
+                chipBrand = ChipBrand.getByMenuOption(option);
+            } catch (IllegalArgumentException e) {
+                logger.error("Error fetching chip brand: ", e);
+            }
+        }
+        chips.setName(chipBrand.getBrandName());
+        chips.setPrice(1.5);
+        chips.setSize(Size.SMALL);
+        chips.setCalories(130);
     }
 
     /**
@@ -30,23 +56,9 @@ public class AddChipsCommand implements Command {
      */
     @Override
     public void execute() {
-        Food chips = new Chips();
-        chips.setPrice(1.5);
-        chips.setCalories(120);
-        String message = String.format("Chips added to your order (Order Number: %d)\nPrice: $%.2f\nCalories: %.2f",
-                order.getOrderNumber(), chips.getPrice(), chips.getCalories());
-        logger.info(message);
-        printChipsAddedToOrder(chips, message);
-    }
-
-    /**
-     * Adds the chips to the order and prints a message.
-     *
-     * @param chips the chips to be added
-     * @param message the message to be printed
-     */
-    private void printChipsAddedToOrder(Food chips, String message) {
         order.getCart().add(chips);
-        Utility.print.accept(message);
+        String message = String.format("\nChips added to your order (Order Number: %d)\nName: %s\nPrice: $%.2f\nCalories: %.2f",
+                order.getOrderNumber(), chips.getName(), chips.getPrice(), chips.getCalories());
+        logger.info(message);
     }
 }
