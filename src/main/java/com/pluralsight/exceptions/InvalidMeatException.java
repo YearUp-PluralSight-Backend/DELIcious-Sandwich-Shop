9f050546c6cfac223cd6404826c546a17cd192ab
Yiming0576 <Yiming0576@gmail.com>
2024-11-12 10:46:47 -0500

    add ConstantValue class to store the static value

---
+++ src/main/java/com/pluralsight/exceptions/InvalidMeatException.java
@@ -0,0 +1,15 @@
+package com.pluralsight.exceptions;
+
+public class InvalidBreadException extends Exception{
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public InvalidBreadException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/pluralsight/utils/ConstantValue.java
@@ -0,0 +1,44 @@
+package com.pluralsight.utils;
+
+public class ConstantValue {
+
+    public static final String ORDER_MENU =
+            """
+            ==================================================
+                       💖 SANDWICH HAVEN - ORDER SCREEN 💖
+            ==================================================
+           
+                      🥪  BUILD YOUR PERFECT ORDER  🥪
+            --------------------------------------------------
+           
+            Select an option:
+           
+              🍞 1) Add Sandwich
+              🥤 2) Add Drink
+              🍟 3) Add Chips
+              🛒 4) Checkout
+              ❌ 0) Cancel Order - delete the order and return to the home screen
+           
+            ==================================================
+                         ❤️ THANK YOU FOR ORDERING ❤️
+            ==================================================
+           """;
+
+    public static final String HOME_MENU =
+            """
+            ============================================
+                    WELCOME TO SANDWICH HAVEN
+            ============================================
+    
+                        (  🍞 🥬 🥓  )
+                        DELICIOUS SANDWICHES
+    
+            Please choose an option:
+    
+            1. New Order
+            2. Exit
+    
+            ============================================
+            """;
+
+}
