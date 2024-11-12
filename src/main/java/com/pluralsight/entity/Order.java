18d5821219d09771552b58c1187af88ebcc123d9
Yiming0576 <Yiming0576@gmail.com>
2024-11-12 08:59:01 -0500

    add orderNumber and createTime on my Order Class

--- src/main/java/com/pluralsight/entity/Order.java
+++ src/main/java/com/pluralsight/entity/Order.java
@@ -1,22 +1,48 @@
 package com.pluralsight.entity;
 
+import com.pluralsight.entity.otherfood.Chips;
+import com.pluralsight.entity.otherfood.Drink;
+import com.pluralsight.entity.sandwich.Sandwich;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
 
 @Data
 public class Order {
 
+    private Long orderNumber;
     private List<Food> cart;
-    private double totalPrice;
-    private int NumberOfItems;
-    private int numberOfDrinks;
-    private int numberOfChips;
-    private int numberOfSandwiches;
+    private LocalDateTime createTime;
 
     public Order() {
         cart = new ArrayList<>();
 
     }
+
+    public double calculateTotalPrice() {
+        return cart.stream().mapToDouble(Food::getPrice).sum();
+    }
+
+    public long getNumberOfChips() {
+        return cart.stream().filter(food -> food instanceof Chips).count();
+    }
+
+    public long getNumberOfDrinks() {
+        return cart.stream().filter(food -> food instanceof Drink).count();
+    }
+
+    public long getNumberOfSandwiches() {
+        return cart.stream().filter(food -> food instanceof Sandwich).count();
+    }
+
+    public long getNumberOfItems() {
+        return cart.size();
+    }
+
+
+
+
 }
