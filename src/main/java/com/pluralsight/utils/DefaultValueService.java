975cb57e266f6fbf9dffad5cc2758aa4678e8cd5
Yiming0576 <Yiming0576@gmail.com>
2024-11-13 16:50:12 -0500

    calculate the price and calories based on the size

---
+++ src/main/java/com/pluralsight/utils/DefaultValueService.java
@@ -0,0 +1,25 @@
+package com.pluralsight.utils;
+
+
+import com.pluralsight.entity.sandwich.toppings.Size;
+import com.pluralsight.exceptions.InvalidIngredientException;
+
+public class DefaultValueService {
+    public static double calculatePrice(ConstantValue.IngredientInfo info, Size size) throws InvalidIngredientException {
+        return switch (size.getValue()) {
+            case 4 -> info.price4Inch();
+            case 8 -> info.price8Inch();
+            case 12 -> info.price12Inch();
+            default -> throw new InvalidIngredientException("Invalid sandwich size");
+        };
+    }
+
+    public static double calculateCalories(ConstantValue.IngredientInfo info, Size size) throws InvalidIngredientException {
+        return switch (size.getValue()) {
+            case 4 -> info.calories();
+            case 8 -> info.calories() * 1.2;
+            case 12 -> info.calories() * 1.4;
+            default -> throw new InvalidIngredientException("Invalid sandwich size");
+        };
+    }
+}
