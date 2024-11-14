5bd20b76b26ea83358a290a89bcfe7215fd1a128
Yiming0576 <Yiming0576@gmail.com>
2024-11-14 08:37:02 -0500

    completed the checkout and cancel functions

--- src/main/java/com/pluralsight/service/commands/CancelOrderCommand.java
+++ src/main/java/com/pluralsight/service/commands/CancelOrderCommand.java
@@ -1,7 +1,6 @@
 package com.pluralsight.service.commands;
 
 import com.pluralsight.entity.Order;
-import com.pluralsight.gui.HomeScreen;
 import com.pluralsight.service.Command;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,8 +10,9 @@ public class CancelOrderCommand implements Command {
     private final Logger logger = LogManager.getLogger(CancelOrderCommand.class);
 
     public CancelOrderCommand(Order order) {
-        order = new Order();
-        logger.info("Cart is clean up! {}", order.getCart());
+        logger.info("Order is canceled!");
+        order = null;
+        logger.info("order : {}", order);
     }
 
     /**
@@ -20,6 +20,5 @@ public class CancelOrderCommand implements Command {
      */
     @Override
     public void execute() {
-        logger.info("Order is canceled!");
     }
 }
--- src/main/java/com/pluralsight/service/commands/CheckoutCommand.java
+++ src/main/java/com/pluralsight/service/commands/CheckoutCommand.java
@@ -1,12 +1,13 @@
 package com.pluralsight.service.commands;
 
 import com.pluralsight.entity.Order;
+import com.pluralsight.repository.GenerateReceipt;
 import com.pluralsight.service.Command;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Command to checkout an order.
+ * Command to check out an order.
  */
 public class CheckoutCommand implements Command {
 
@@ -27,7 +28,11 @@ public class CheckoutCommand implements Command {
      */
     @Override
     public void execute() {
-        order.calculateTotalPrice();
-        logger.info("Total price is {}", order.getTotalPrice());
+        logger.info("Total price is {}", order.calculateTotalPrice());
+        logger.info("Total calories is {}", order.getTotalCalories());
+//        GenerateReceipt.printReceiptToConsole(order);
+        boolean successful = GenerateReceipt.writeReceiptToFile(order);
+        order = new Order();
+        logger.info("new order : {}", order);
     }
 }
